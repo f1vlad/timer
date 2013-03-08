9eb3504ed577e6b45bf33ab026a63b6987779bbb
f1vlad <kulchitski@gmail.com>
2013-03-08 00:09:54 -0800

    Refactor
    g

--- timer.js
+++ timer.js
@@ -1,37 +1,32 @@
 (function() {
-    var beginTriger = 'convert_lead',
-        beginTime,
-        endTrigger = '#lead-convert-success',
-        endTime,
-        clicks = 0;
+    var lib = {
+        beginTriger: 'convert_lead',
+        beginTime: 0,
+        endTrigger: '#lead-convert-success',
+        endTime: 0,
+        clicks: -2,
+        start: function() {
+            beginTime = new Date().getTime();
+            this.beginTestAlert();
+        },
+        end: function() {
+            endTime = new Date().getTime();
+            $('#lead-convert-success').attr('id', '');
+            this.showResults();
+        },
+        lapTime: function() {
+            return Math.round( ((endTime - beginTime ) / 1000)*100 )/100;
+        },
+        beginTestAlert: function() {throwMessage('<strong>Note!</strong> You entered a test zone', 'success', true);},
+        showResults: function() {throwMessage('<strong>Test completed!</strong> It took you ' + this.lapTime() + ' seconds and ' + this.clicks + ' clicks to complete this task', 'success', false);}
+    };
+    window.UITEST = lib;
+
+    var ui = window.UITEST;
 
     $('#sidecar').bind('click', '*', function(e){
-    	($(e)[0]['target']['className'] === beginTriger) ? start() : false;
-    	($(endTrigger).length === 1) ? end() : false;
-    	var status = ($('#sugarcrm').find('#lead-convert-success').length===1) ? true : false;
-    	clicks += 1;
+        ($(e)[0]['target']['className'] === ui.beginTriger) ? ui.start() : false;
+        ($(ui.endTrigger).length === 1) ? ui.end() : false;
+        ui.clicks += 1;
     });
-
-    var start = function () {
-        beginTime = new Date().getTime();
-        beginTestAlert();
-    };
-
-    var end = function () {
-        endTime = new Date().getTime();
-        showResults();
-    };
-
-    var lapTime = function () {
-    	return (endTime - beginTime ) / 1000;
-    	
-    };
-
-    var beginTestAlert = function () {
-    	throwMessage('<strong>Note!</strong> You entered a test zone', 'success', true);
-    };
-
-    var showResults = function () {
-        throwMessage('<strong id="lead-convert-success">Test completed!</strong> It took you ' + lapTime() + ' seconds and ' + clicks + ' clicks to complete this task', 'success', false);
-    };
 })();
