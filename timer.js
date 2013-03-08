a9b359074c9a9f39a2170ec30cd8923200d8e25c
f1vlad <kulchitski@gmail.com>
2013-03-07 23:41:15 -0800

    Protect from global scope

--- timer.js
+++ timer.js
@@ -1,15 +1,16 @@
-var beginTriger = 'convert_lead',
-    beginTime,
-    endTrigger = '#lead-convert-success',
-    endTime,
-    clicks = 0;
+(function() {
+    var beginTriger = 'convert_lead',
+        beginTime,
+        endTrigger = '#lead-convert-success',
+        endTime,
+        clicks = 0;
 
-	$('#sidecar').bind('click', '*', function(e){
-		($(e)[0]['target']['className'] === beginTriger) ? start() : false;
-		($(endTrigger).length === 1) ? end() : false;
-		var status = ($('#sugarcrm').find('#lead-convert-success').length===1) ? true : false;
-		clicks += 1;
-	});
+    $('#sidecar').bind('click', '*', function(e){
+    	($(e)[0]['target']['className'] === beginTriger) ? start() : false;
+    	($(endTrigger).length === 1) ? end() : false;
+    	var status = ($('#sugarcrm').find('#lead-convert-success').length===1) ? true : false;
+    	clicks += 1;
+    });
 
     var start = function () {
         beginTime = new Date().getTime();
@@ -27,9 +28,10 @@ var beginTriger = 'convert_lead',
     };
 
     var beginTestAlert = function () {
-		throwMessage('<strong>Note!</strong> You entered a test zone', 'success', true);
+    	throwMessage('<strong>Note!</strong> You entered a test zone', 'success', true);
     };
 
     var showResults = function () {
         throwMessage('<strong id="lead-convert-success">Test completed!</strong> It took you ' + lapTime() + ' seconds and ' + clicks + ' clicks to complete this task', 'success', false);
     };
+})();
