433c7e09f783666505cfe1a58f56f4aea770d9c4
f1vlad <kulchitski@gmail.com>
2013-03-09 00:11:55 -0800

    Add placeholder controller

--- timer.js
+++ timer.js
@@ -5,6 +5,7 @@
         endTrigger: '#lead-convert-success',
         endTime: 0,
         clicks: -2,
+        controller: '<div class="uitest-controller" style="position: absolute; width:auto; height:28px; background: #ccc; border:1px solid #bbb; border-radius: 3px; padding: 6px 10px; right: 0; top:50%; box-shadow:inset 0px -19px 7px #aaa"><a href="#" style="line-height:27px; cursor:ew-resize">UI<strong class="toggle hide">TEST</strong></a> <span class="toggle hide">Contact info: <input type="text" name="contact" placeholder="Name, email"><input type="button" class="btn start" value="start"><input type="button" disabled="disabled" class="btn stop" value="stop"></span></div>',
         start: function() {
             this.beginTime = new Date().getTime();
             this.beginTestAlert();
@@ -21,6 +22,7 @@
         showResults: function() {throwMessage('<strong>Test completed!</strong> It took you ' + this.lapTime() + ' seconds and ' + this.clicks + ' clicks to complete this task', 'success', false);}
     };
     window.UITEST = lib;
+    $('body').append(window.UITEST.controller);
 
     var ui = window.UITEST;
 
@@ -28,5 +30,10 @@
         ($(e)[0]['target']['className'] === ui.beginTriger) ? ui.start() : false;
         ($(ui.endTrigger).length === 1) ? ui.end() : false;
         ui.clicks += 1;
+    });
+
+    $('.uitest-controller > a').live('click', function(e){
+        $('.uitest-controller .toggle').toggleClass('hide');
+        return false;
     });
 })();
