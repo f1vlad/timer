dd51463e31502d0ac35966658af3b480b6bd511f
f1vlad <kulchitski@gmail.com>
2013-03-17 01:19:56 -0700

    Make both auto and manual test work

--- timer.js
+++ timer.js
@@ -1,15 +1,18 @@
 (function() {
     var lib = {
         testAuto: false, // auto test will start automatically based on start|step html selector trigger
+        testInProgress: false,
         beginTriger: 'convert_lead', // make optional
         beginTime: 0,
         endTrigger: '#lead-convert-success',  // make optional
         endTime: 0,
-        clicks: -2,
+        clicks: 0,
+        trace: {},
         remoteControll: '<div class="uitest-remote-control" style="position: absolute; width:auto; height:28px; background: #ccc; border:1px solid #bbb; border-radius: 3px; padding: 6px 10px; right: 0; top:50%; box-shadow:inset 0px -19px 7px #aaa"><a href="#" style="line-height:27px; cursor:ew-resize">UI<strong class="toggle hide">TEST</strong></a> <span class="toggle hide">Contact info: <input type="text" name="contact" placeholder="Name, email"><input type="button" class="btn start" value="start"><input type="button" disabled="disabled" class="btn stop" value="stop"></span></div>',
         start: function() {
             this.beginTime = new Date().getTime();
             this.beginTestAlert();
+            this.testInProgress = true;
         },
         end: function() {
             this.endTime = new Date().getTime();
@@ -17,7 +20,7 @@
             this.showResults();
         },
         lapTime: function() {
-            return Math.round( ((this.endTime - this.beginTime ) / 1000)*100 )/100;
+            return  Math.round( ((this.trace[this.clicks].timestamp - this.trace[1].timestamp ) / 1000)*100 )/100;
         },
         beginTestAlert: function() {throwMessage('<strong>Note!</strong> You entered a test zone', 'success', true);},
         showResults: function() {throwMessage('<strong>Test completed!</strong> It took you ' + this.lapTime() + ' seconds and ' + this.clicks + ' clicks to complete this task', 'success', false);}
@@ -27,26 +30,33 @@
 
 
 
-// TODO: when start button clicked, give timeout to user like 10 seconds time out before test begins, maybe disable all buttons until that starts
-//       show stop button in evident place so that user can quickly halt test
-//       make red button on the left of UITEST label, disable it and make counter right on the button before test starts
-
-
     var ui = window.UITEST;
 
     if(ui.testAuto === true) {
         $('#sidecar').bind('click', '*', function(e){
             ($(e)[0]['target']['className'] === ui.beginTriger) ? ui.start() : false;
             ($(ui.endTrigger).length === 1) ? ui.end() : false;
-            ui.clicks += 1;
+            if(ui.testInProgress === true) {
+                ui.clicks += 1;
+                ui.trace[ui.clicks] = {
+                    'timestamp': new Date().getTime(),
+                    'coordinates': 'false'
+                };                
+            }
         });
     } else {
+        $('#sidecar').bind('click', '*', function(e){
+            ui.clicks += 1;
+            ui.trace[ui.clicks] = {
+                'timestamp': new Date().getTime(),
+                'coordinates': 'false'
+            };
+        });
         $('.uitest-remote-control .btn:not(.disabled)').live('click', function(e){
             if($(this).hasClass('start') === true) {
                 ui.start();
                 $(this).attr('disabled','disabled').next().removeAttr('disabled');
             } else if ($(this).hasClass('stop') === true) {
-                console.log('f');
                 ui.end();
                 $(this).attr('disabled','disabled').prev().removeAttr('disabled');
             }
@@ -57,7 +67,6 @@
         $('.uitest-remote-control .toggle').toggleClass('hide');
         return false;
     });
-
 
 
 
