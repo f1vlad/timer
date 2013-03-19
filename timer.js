f5b536b70a05829ff27e0639cdcec278895f2d97
f1vlad <kulchitski@gmail.com>
2013-03-19 01:44:21 -0700

    Log more info
    g

--- timer.js
+++ timer.js
@@ -2,24 +2,28 @@
     var lib = {
         testAuto: false, // auto test will start automatically based on start|step html selector trigger
         testInProgress: false,
+        testSubject: '',
         beginTime: 0,
         endTime: 0,
         clicks: 0,
         beginTriger: 'convert_lead', // required for auto-start test
         endTrigger: '#lead-convert-success',  // required for auto-start test
         trace: {},
-        remoteControll: '<div class="uitest-remote-control" style="position: absolute; width:auto; height:28px; background: #ccc; border:1px solid #bbb; border-radius: 3px; padding: 6px 10px; right: 0; top:50%; box-shadow:inset 0px -19px 7px #aaa"><a href="#" style="line-height:27px; cursor:ew-resize">UI<strong class="toggle hide">TEST</strong></a> <span class="toggle hide">Contact info: <input type="text" name="contact" placeholder="Name, email"><input type="button" class="btn start" value="start"><input type="button" disabled="disabled" class="btn stop" value="stop"><input type="button" class="btn auto-manual-toggle manual" value="auto off"></span></div>',
+        remoteControll: '<div class="uitest-remote-control" style="position: absolute; width:auto; height:28px; background: #ccc; border:1px solid #bbb; border-radius: 3px; padding: 6px 10px; right: 0; top:50%; box-shadow:inset 0px -19px 7px #aaa"><a href="#" style="line-height:27px; cursor:ew-resize">UI<strong class="toggle hide">TEST</strong></a> <span class="toggle hide">Contact info: <input type="text" name="test-subject" placeholder="Name, email"><input type="button" class="btn start" value="start"><input type="button" disabled="disabled" class="btn stop" value="stop"><input type="button" class="btn auto-manual-toggle manual" value="auto off"></span></div>',
         start: function() {
             this.clicks = 0;
             this.trace = {};
             this.beginTime = new Date().getTime();
             this.beginTestAlert();
+            this.testSubject = $('.uitest-remote-control input[type=text]').val();
+            this.testSubjectMachine = screen.width + 'x' + screen.height + ', ' + navigator.userAgent.toLowerCase(); // TODO: detect if user changed browser resolution
             this.testInProgress = true;
         },
         end: function() {
             this.endTime = new Date().getTime();
             $('#lead-convert-success').attr('id', '');
             this.showResults();
+            $('.uitest-remote-control input[type=text]').attr('value', '');
         },
         lapTime: function() {
             return  Math.round( ((this.trace[this.clicks].timestamp - this.trace[1].timestamp ) / 1000)*100 )/100;
